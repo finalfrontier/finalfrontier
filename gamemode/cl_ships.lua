a4d4a82984f90c905a01101df78b6e656495ff8f
James King <metapyziks@gmail.com>
2012-12-30 16:34:56 +0000

    Fixed error on join

--- gamemode/cl_ships.lua
+++ gamemode/cl_ships.lua
@@ -2,14 +2,18 @@ ships = {}
 
 ships._dict = {}
 
+function ships.AddShip(ship)
+	ships._dict[ship.Name] = ship
+end
+
 function ships.FindByName(name)
 	return ships._dict[name]
 end
 
 net.Receive("InitShipData", function(len)
 	local ship = Ship()
-	ship:ReadFromNet()	
-	ships._dict[name] = ship
+	ship:ReadFromNet()
+	ships.AddShip(ship)
 end)
 
 net.Receive("ShipStateUpdate", function(len)
