d7d438ea7a47154fd5d36911db948ac05b1a2fdc
James King <metapyziks@gmail.com>
2013-06-21 20:10:40 +0100

    Cleared web branch

--- gamemode/init.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("cl_door.lua")
-AddCSLuaFile("cl_room.lua")
-AddCSLuaFile("cl_ship.lua")
-AddCSLuaFile("cl_ships.lua")
-AddCSLuaFile("cl_universe.lua")
-
-include("sv_init.lua")
