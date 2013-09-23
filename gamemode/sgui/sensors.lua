c5943fee3496dd85e90d2391bac1ccda0ad70b09
Saša Barišić <cartman300@net.hr>
2013-09-23 20:59:27 +0200

    Wat

--- gamemode/sgui/sensors.lua
+++ gamemode/sgui/sensors.lua
@@ -83,11 +83,21 @@ function GUI:Inspect(obj)
         if SERVER then
             local min = self._grid:GetMinScale()
             local max = self._grid:GetMaxScale()
-            self._zoomSlider.Value = math.sqrt((self._grid:GetScale() - min) / (max - min))
+
+            if self:GetScreen().Storage.ZoomSliderValue then
+                self._zoomSlider.Value = self:GetScreen().Storage.ZoomSliderValue
+            else
+                self._zoomSlider.Value = math.sqrt((self._grid:GetScale() - min) / (max - min))
+                self:GetScreen().Storage.ZoomSliderValue = self._zoomSlider.Value 
+            end
+
+            self._grid:SetScale(min + math.pow(self._zoomSlider.Value, 2) * (max - min))
+
             function self._zoomSlider.OnValueChanged(slider, value)
                 min = self._grid:GetMinScale()
                 max = self._grid:GetMaxScale()
                 self._grid:SetScale(min + math.pow(value, 2) * (max - min))
+                self:GetScreen().Storage.ZoomSliderValue = value
             end
         end
 
