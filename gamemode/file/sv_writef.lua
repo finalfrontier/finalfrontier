198d2822dff4d93cd318f7d9513e8ebbf634b554
Geroge <spartan322@Live.com>
2014-01-28 16:06:36 -0500

    sv_writef.lua now writes server files

--- gamemode/file/sv_writef.lua
+++ gamemode/file/sv_writef.lua
@@ -2,9 +2,35 @@ FILE = {}
 fileName = nil
 filePath = nil
 fileBase = nil
+fileWrite = nil
 
-function FILE:WriteFile()
+function FILE:Path(pathway)
+    if string.EndsWith(pathway, "\\")
+       pathway = string.TrimRight(pathway, "\\")
+    end
+    filePath = pathway 
+end
+
+function FILE:Name(name)
+   fileName = name 
+end
+
+function FILE:Base(base)
+   fileBase = base
+end
+
+
+
+if Server then
+    function FILE:WriteFile()
+        file.Write(fileName, fileWrite)
+    end
     
-    
+    -- These functions should be called last out of all of these
+    function FILE:Write(writeTo)
+        fileWrite = writeTo
+        self:WriteFile()
+    end
     
 end
+
