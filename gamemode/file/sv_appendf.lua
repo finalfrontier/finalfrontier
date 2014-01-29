f20481443c0c60fff5801414adb32f90fe6ee933
Geroge <spartan322@Live.com>
2014-01-29 13:32:28 -0500

    sv_appendf.lua Fixed up.

--- gamemode/file/sv_appendf.lua
+++ gamemode/file/sv_appendf.lua
@@ -1,5 +1,32 @@
 FileA = {}
 fileName = nil
 filePath = nil
-fileBase = nil
+fileBase = "DATA"
 fileAppend = nil
+
+function FILEA:Name(name)
+  fileName = name
+end
+
+function FILEA:Path(pathway)
+  filePath = pathway
+end
+
+-- Completly Useless but included for sake of base call needed
+function FILEA:Base()
+  fileBase = "DATA"
+end
+
+if Server then
+  
+    function FILEA:Append()
+        file.Append(filePath..fileName, fileAppend)
+        fileCheck = file.Read(filePath..fileName, fileBase)
+        if fileCheck != nil then 
+            fileCheck = string.format(fileCheck.."%", "\n"..fileAppend)
+            file.Write(filePath..fileName, fileCheck)
+        end
+       end
+    end
+  
+end
