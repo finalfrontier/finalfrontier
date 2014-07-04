ff15330ae0fa55b6acf8f513a89f7eae21b88a2b
Metapyziks <metapyziks@gmail.com>
2014-07-04 20:32:21 +0100

    Increased ship acceleration for testing purposes

--- gamemode/systems/piloting.lua
+++ gamemode/systems/piloting.lua
@@ -38,7 +38,7 @@ if SERVER then
     local SNAPPING_THRESHOLD_POS = 1.0 / 16384.0
     local SNAPPING_THRESHOLD_VEL = 1.0 / 16384.0
 
-    local ACCELERATION_PER_POWER = 1.0 / 800.0
+    local ACCELERATION_PER_POWER = 1.0 / 400.0
 
     local function findAccel1D(a, u, s)
         local v = math.sqrt(2 * a * math.abs(s)) * math.sign(s)
