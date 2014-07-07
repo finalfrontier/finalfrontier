ea6d14ce6f74fa2bc093eb0f7fe558fd0032a38c
Geroge <Spartan322@live.com>
2014-07-07 12:12:02 -0400

    Update and rename sh_new-teams.lua to sh_new_teams.lua

--- gamemode/sh_new_teams.lua
+++ gamemode/sh_new_teams.lua
@@ -15,7 +15,7 @@
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with Final Frontier. If not, see <http://www.gnu.org/licenses/>.
 
-if SERVER then AddCSLuaFile("sh_new-teams.lua") end
+if SERVER then AddCSLuaFile("sh_new_teams.lua") end
 
 TEAM_LIMIT = 20
 team_groups = {}
