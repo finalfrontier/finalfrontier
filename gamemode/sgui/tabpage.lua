7938f8d9e6da1b39065551987de229de00141b39
Geroge <Spartan322@live.com>
2014-07-12 19:53:57 -0400

    Create tabpage.lua

---
+++ gamemode/sgui/tabpage.lua
@@ -0,0 +1,43 @@
+local BASE = "container"
+
+GUI.BaseName = BASE
+
+GUI.Color = Color(191, 191, 191, 255)
+
+GUI._tpages = nil
+GUI._currenttpage = 0
+
+GUI.TabHeight = 48
+GUI.TabMargin = 8
+
+page = {}
+
+function GUI:Initialize()
+	self.Super[BASE].Initialize(self)
+	
+	self._tpages = {}
+end
+
+function GUI:AddMenu()
+	_lastPage = #self.tpages
+	self._tpages[_lastPage].TabMenu = sgui.Create(self:GetScreen(), "tabmenu")
+	self._tpages[_lastPage].TabMenu:SetSize(self:GetWidth() - self.TabMargin * 2, self.TabHeight)
+	self._tpages[_lastPage].TabMenu:SetCentre(self:GetWidth() / 2, self.TabHeight / 2 + self.TabMargin)
+	self._tpages[_lastPage].Tabs = {}
+	self._tpages[_lastPage].Tabs[page[0].number] = self._tpages[_lastPage].TabMenu:AddTab(page[0].name)
+	self._tpages[_lastPage].Tabs[page[1].number] = self._tpages[_lastPage].TabMenu:AddTab(page[1].name)
+	self._tpages[_lastPage].Tabs[page[2].number] = self._tpages[_lastPage].TabMenu:AddTab(page[2].name)
+	self._tpages[_lastPage].Tabs[page[3].number] = self._tpages[_lastPage].TabMenu:AddTab(page[3].name)
+end
+
+function GUI:SetPage(num, text)
+	page[num] = {}
+	page[num].number = num
+	page[num].name = text
+end
+
+function GUI:GetCurrentTabPage()
+	return self._currenttpage
+end
+
+
