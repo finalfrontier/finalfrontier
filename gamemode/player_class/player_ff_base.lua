d9396d88375a6106f5b71e420e6ee004dfccca22
Geroge <Spartan322@live.com>
2014-07-13 17:40:30 -0400

    Added Player Base File
    Allows easier

--- gamemode/player_class/player_ff_base.lua
+++ gamemode/player_class/player_ff_base.lua
@@ -1,56 +1,78 @@
 if SERVER then AddCSLuaFile("player_ff_base.lua") end
 
-local PLAYER = {}
+Class_Manager = {}
 
-PLAYER.WalkSpeed            = 175
-PLAYER.RunSpeed             = 250
+Class_Manager.Class = {}
 
-PLAYER.DisplayName = ""
+Class_Manager.HeldItems = {}
 
-local _models = {}
+local _currentClass = #self.Class
 
-if SERVER then
-    function PLAYER:Init()
-        team.AutoAssign(self.Player)
-    end
+local _classname = ""
 
-    function PLAYER:Spawn()
-        local ship = team.GetShip(self.Player:Team())
-        local pad = table.Random(ship:GetSystem("transporter"):GetRoom():GetTransporterPads())
-
-        self.Player:SetPos(pad)
-        self.Player:SetShip(ship)
-
-        self.Player:SetCanWalk(true)
-
-        TeleportArriveEffect(self.Player, self.Player:GetPos())
-    end
-
-    function PLAYER:SetModel()
-        self.Player:SetModel(table.Random(_models))
-    end
-
-    function PLAYER:Loadout()
-        self.Player:Give("weapon_crowbar")
-        self.Player:Give("weapon_ff_repair_tool")
-    end
+--Use Before AddClass
+function Class_Manager.SetItems(items)
+    self.HeldItems = items
 end
 
-function PLAYER:SetupDataTables()
-    local ply = self.Player or self
+function Class_Manager.AddClass(classname, displayname, walkspeed, runspeed, models)
+    self.Class[_currentClass] = {}
+    self.Class[_currentClass].DisplayName = displayname
+    self.Class[_currentClass].WalkSpeed = walkspeed
+    self.Class[_currentClass].RunSpeed = runspeed
+    
+    _classname = classname
+    
+    local _models = models
+    
+    if SERVER then
+        function self.Class[_currentClass]:Init()
+            team.AutoAssign(self.Player)
+        end 
+    
+        function self.Class[_currentClass]:Spawn()
+            local ship = team.GetShip(self.Player:Team())
+            local pad = table.Random(ship:GetSystem("transporter"):GetRoom():GetTransporterPads())
 
-    ply:NetworkVar("String", 0, "ShipName")
+            self.Player:SetPos(pad)
+            self.Player:SetShip(ship)
 
-    ply:NetworkVar("Int", 0, "RoomIndex")
+            self.Player:SetCanWalk(true)
 
-    ply:NetworkVar("Bool", 0, "UsingScreen")
+            TeleportArriveEffect(self.Player, self.Player:GetPos())
+        end
+        
+        function self.Class[_currentClass]:SetModel()
+            self.Player:SetModel(table.Random(_models))
+        end
 
-    ply:NetworkVar("Entity", 0, "CurrentScreen")
-    ply:NetworkVar("Entity", 1, "OldWeapon")
+        function self.Class[_currentClass]:Loadout()
+            if not Class_Manager.HeldItems then
+                self.Player:Give("weapon_crowbar")
+                self.Player:Give("weapon_ff_repair_tool")
+            else
+                for k,v in pairs(Class_Manager.HeldItems) do
+                    self.Player:Give(v)
+                end
+        end
+    end
 
-    ply._permissions = ply:NetworkTable(0, "Permissions")
+    function self.Class[_currentClass]:SetupDataTables()
+        local ply = self.Player or self
+
+        ply:NetworkVar("String", 0, "ShipName")
+    
+        ply:NetworkVar("Int", 0, "RoomIndex")
+    
+        ply:NetworkVar("Bool", 0, "UsingScreen")
+
+        ply:NetworkVar("Entity", 0, "CurrentScreen")
+        ply:NetworkVar("Entity", 1, "OldWeapon")
+
+        ply._permissions = ply:NetworkTable(0, "Permissions")
+    end
+    
+    SetupPlayerDataTables = self.Class[_currentClass].SetupDataTables
 end
 
-SetupPlayerDataTables = PLAYER.SetupDataTables
-
-player_manager.RegisterClass("player_ff_base", PLAYER, "player_base")
+player_manager.RegisterClass("player_ff_".._classname, self.Class[_currentClass], "player_".._classname)
